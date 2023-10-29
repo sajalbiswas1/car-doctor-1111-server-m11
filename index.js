3b8170852931e9712c7f33287a7cf327f378d342
Programing Hero <sajalb1899@gmail.com>
2023-10-29 20:12:53 +0600

    booking update spacice property change confirm

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
         await client.connect();
 
         const servicesCollection = client.db("carDoctor").collection("services")
+        const bookingCollection = client.db("carDoctor").collection("bookings")
 
         app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find();
@@ -39,13 +40,50 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const option = {
-                projection: { title: 1, price: 1, service_id: 1 },
+                projection: { title: 1, price: 1, service_id: 1,img:1 },
             }
             const result = await servicesCollection.findOne(query,option)
             res.send(result)
-
         })
 
+        //booking 
+        app.get('/bookings', async(req,res)=>{
+           let query = {}
+           if(req.query?.email){
+            query = {email: req.query.email}
+           }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result)
+        })
+
+
+        app.post('/bookings', async(req,res)=>{
+            const booking = req.body;
+            console.log(booking)
+            const result = await bookingCollection.insertOne(booking)
+            res.send(result)
+        })
+
+        app.patch('/bookings/:id', async(req,res)=>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const updateBooking = req.body;
+            console.log(updateBooking)
+            const updateDoc = {
+                $set: {
+                    status: updateBooking.status
+                }
+            }
+            const result = await bookingCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+        app.delete('/bookings/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
